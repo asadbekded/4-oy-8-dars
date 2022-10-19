64fb4ee3be6afa0a54c23d92097f701eba9493dc
asadbekded <asadbek171@gmail.com>
2022-10-19 19:47:07 +0500

    Add new change html

--- js/main.js
+++ js/main.js
@@ -1,57 +1,104 @@
-let elList = document.querySelector('.js-list') 
-let elForm = document.querySelector('.js-form')
-let elInp = document.querySelector('.js-inp')
+const elList = document.querySelector('.js-list') 
+const elForm = document.querySelector('.js-form')
+const elInp = document.querySelector('.js-inp')
+const elGroupBtn = document.querySelector('.buttons-page')
+const elPrex = document.querySelector('.prex-btn')
+const elNext = document.querySelector('.next-btn')
+const elSel = document.querySelector('.js-select')
+
+let active = 1
 // ----------------------elon qilinib boldi htmldan -------------
+
+elForm.addEventListener('click', function(evt) {
+   evt.preventDefault('')
+   getMovie()
+})
+
+
 var itemFragment = new DocumentFragment()
-function renderTodo(arr,node) {
-   if(arr.length > 0){
+const renderMovie = (array,node) => {
+   if(array.Response == 'True'){
       node.innerHTML = "";
-      arr.forEach(el => {
+      array.Search.forEach((item) => {
          const newItem = document.createElement('li')
          const newTitle = document.createElement('h5')
          const newImg = document.createElement('img')
          const newSpan = document.createElement('span')
          const newSpan1 = document.createElement('span')
-
-         newTitle.textContent = el.Title
-         newImg.src = el.Poster
+         const newSpan2 = document.createElement('span')
+   
+         newImg.src = item.Poster
          newImg.width = '270'
          newImg.height = '300'
-         newSpan.textContent = `Year : ${el.Year}`
-         newSpan1.textContent = `Type : ${el.Type}`
-
-         newItem.appendChild(newTitle)
+         newTitle.textContent = item.Title
+         newSpan.textContent = `Year : ${item.Year}`
+         newSpan1.textContent = `Type : ${item.Type}`
+         newSpan2.textContent = `Id : ${item.imdbID}`
+   
          newItem.appendChild(newImg)
+         newItem.appendChild(newTitle)
          newItem.appendChild(newSpan)
          newItem.appendChild(newSpan1)
          itemFragment.appendChild(newItem)
       })
       node.appendChild(itemFragment)
    }
+
+   if(active == 1){
+      elPrex.setAttribute('disabled', true)
+   }else{
+      elPrex.removeAttribute('disabled')
+   }
+
+   let totals = Math.ceil(array.totalResults / 10)
+
+   if(active == totals){
+      elNext.setAttribute('disabled', true)
+   }else{
+      elNext.removeAttribute('disabled')
+   }
 }
 
-elForm.addEventListener('click', function(evt) {
-   evt.preventDefault('')
-   let elInpVal = elInp.value
-   async function getTodo() {
-      const response = await fetch(`https://omdbapi.com/?apikey=fc8e9a22&s=${elInpVal}&`)
-      const data = await response.json()
-      let datas = data.Search
-      renderTodo(datas,elList)
-   }
-   getTodo()
+async function getMovie() {
+   const response = await fetch(`https://omdbapi.com/?apikey=fc8e9a22&s=${elInp.value}&page=${active}&type=${elSel.value}`)
+   const data = await response.json()
+   renderMovie(data, elList)
+}
+
+elSel.addEventListener('change', function () {
+   getMovie()
 })
+
+elGroupBtn.addEventListener('click', function(evt) {
+   if(evt.target.matches('.prex-btn')){
+      active--
+      getMovie()
+   }
+   if(evt.target.matches('.next-btn')){
+      active++
+      getMovie()
+   }
+})
+
+
+
+
+
+
+
+
 //---------------------- mode btn code begin----------------
 const elMode = document.querySelector('.mode-btn')
-var theme = false
 
+var theme = false
 elMode.addEventListener('click', function(evt){
    theme = !theme
-   window.localStorage.setItem('theme', theme? 'dark': 'light')
+   var resulMode = theme? 'dark': 'light';
+   window.localStorage.setItem('theme', resulMode)
    changeTheme()
 })
 
-var changeTheme = (el => {
+function changeTheme () {
    if(window.localStorage.getItem('theme') == 'dark'){
       document.body.style.backgroundColor = '#333'
       elMode.textContent = 'Light'
@@ -62,6 +109,6 @@ var changeTheme = (el => {
       elMode.textContent = 'Dark'
       elMode.setAttribute('class', 'btn btn-dark');
    }
-})
+}
 changeTheme()
 //---------------------- mode btn code finished----------------
